f3fdea5a0486c284509d99e65e4094079f217df4
Dennycom <177092101+Dennycom@users.noreply.github.com>
2025-04-03 17:06:06 -0500

    Create function to get computer choice

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,18 @@
 console.log("Hello World!");
+
+function getComputerChoice(){
+    let computerTurn; //random number played by computer
+    let minVal = 1; // lower value
+    let maxVal = 3; //upper value
+    
+    computerTurn = Math.floor(Math.random() * (maxVal-MinVal + 1)) + minVal //get random no 
+    //between 1 and 3
+
+    if (computerTurn === 1) {
+        return "Rock";
+    }else if (computerTurn === 2) {
+        return "Paper";
+    }else
+        return "Scissors";
+
+}
