fd778adc361407d20614388b8b84eb55271f9c3a
Dennycom <177092101+Dennycom@users.noreply.github.com>
2025-04-03 21:29:33 -0500

    create the getHumanChoice function and backtest it.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -5,7 +5,7 @@ function getComputerChoice(){
     let minVal = 1; // lower value
     let maxVal = 3; //upper value
     
-    computerTurn = Math.floor(Math.random() * (maxVal-MinVal + 1)) + minVal //get random no 
+    computerTurn = Math.floor(Math.random() * (maxVal-minVal + 1)) + minVal //get random no 
     //between 1 and 3
 
     if (computerTurn === 1) {
@@ -15,4 +15,30 @@ function getComputerChoice(){
     }else
         return "Scissors";
 
+
 }
+alert(getComputerChoice()); // test if this function is working fine on webpage.
+console.log(getComputerChoice()) // test if function is working fine on console
+
+let randomNum = getComputerChoice(); //test if function is working
+console.log(randomNum);
+alert(randomNum); 
+
+ function getHumanChoice(){
+    //let userInput = prompt("Rock Paper or Scissors?" , " " );
+    let userInput = prompt("Rock, Paper or Scissors?" , " " ); //ask user to choose between Rock, Paper or Scissors
+   
+    if (userInput == "Rock") {        
+        return "Rock";
+    }else if (userInput == "Paper") {
+        return "Paper";
+    }else {
+        return "Scissors";
+    }
+}
+ let testfunction = getHumanChoice(); //assign a variable to the result of calling getHumanChoice function
+ console.log(testfunction);  //test to see if getHuman function is working properly-what does it output in the console?
+
+ 
+
+  
